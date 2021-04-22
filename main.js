b46cd91b570b66c73b614ec302649044a69c6edd
amandasmcvey <amandamcvey11@gmail.com>
2021-04-22 16:37:41 -0500

    JS corrections - corrects bug on page reload

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 $(document).ready(function () {
-  window.addEventListener("hashchange", function (){
-    if(!location.hash) {
-      location.hash = "#home";
-    }
-  
-    var pageName = location.hash.substr(1);
-  
-    $("#body").load(pageName + ".html");
-    $("#header").html(pageName);
-  });
+  navigate();
 });
+
+$(window).on('hashchange', navigate);
+
+function navigate(){
+  if(!location.hash) {
+    location.hash = "#home";
+  }
+
+  var pageName = location.hash.substr(1);
+
+  $("#body").load(pageName + ".html");
+  $("#header").html(pageName);
+}
